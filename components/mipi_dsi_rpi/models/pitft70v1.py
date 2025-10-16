9c296f37a4fb1954a81f71760284b32e569bff04
Oliver Kleinecke <okleinecke@web.de>
2025-10-16 12:12:46 +0200

    Add PITFT70 driver configuration for display

---
+++ components/mipi_dsi_rpi/models/pitft70v1.py
@@ -0,0 +1,23 @@
+from esphome.components.mipi import DriverChip
+import esphome.config_validation as cv
+
+# fmt: off
+DriverChip(
+    "PITFT70",
+    width=800,
+    height=480,
+    lanes=1,
+    color_depth=16,
+    byte_order="little_endian",
+    hsync_back_porch=48,
+    hsync_pulse_width=48,
+    hsync_front_porch=160,
+    vsync_back_porch=8,
+    vsync_pulse_width=6,
+    vsync_front_porch=23,
+    pclk_frequency="27.777MHz",
+    lane_bit_rate="1Gbps",
+    swap_xy=cv.UNDEFINED,
+    color_order="RGB",
+    initsequence=[],
+)
