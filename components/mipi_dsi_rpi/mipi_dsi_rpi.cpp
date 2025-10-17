2cfc660a5df4502b997c37db597a776cdc9e37de
Oliver Kleinecke <okleinecke@web.de>
2025-10-17 17:13:48 +0200

    Update mipi_dsi_rpi.cpp

--- components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
+++ components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
@@ -121,7 +121,7 @@ void MIPI_DSI_RPI::setup() {
   delay(25);
   
   // Configure bridge via DSI
-  dsi_write(0x0210, 0x00000007);  // DSI_LANEENABLE
+  dsi_write(0x0210, 0x00000003);  // DSI_LANEENABLE
   dsi_write(0x0164, 0x00000005);  // PPI_D0S_CLRSIPOCOUNT
   dsi_write(0x0168, 0x00000005);  // PPI_D1S_CLRSIPOCOUNT
   dsi_write(0x0144, 0x00000000);  // PPI_D0S_ATMR
