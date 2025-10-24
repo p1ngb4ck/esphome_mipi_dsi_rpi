0b4a4493ac5059ec5cef46107d3017470e86580f
Oliver Kleinecke <okleinecke@web.de>
2025-10-25 00:00:17 +0200

    Update pitft70v1.py

--- components/mipi_dsi_rpi/models/pitft70v1.py
+++ components/mipi_dsi_rpi/models/pitft70v1.py
@@ -6,7 +6,7 @@ DriverChip(
     "PITFT70V1",
     width=800,
     height=480,
-    lanes=2,
+    lanes=1,
     color_depth=24,
     pixel_format="MIPI_DSI_FMT_RGB888",
     byte_order="little_endian",
