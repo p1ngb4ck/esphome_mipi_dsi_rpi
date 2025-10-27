85aece8b7da0a8fdbcde11bea145f6a0451d97b1
Oliver Kleinecke <okleinecke@web.de>
2025-10-27 03:26:48 +0100

    Update mipi_dsi_rpi.cpp

--- components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
+++ components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
@@ -101,6 +101,21 @@ void MIPI_DSI_RPI::setup() {
   buffer[0] = 0x85;
   this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
   delay(100);
+  buffer[0] = 0x83;
+  buffer[1] = 0x00;
+  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
+  delay(25);
+  buffer[0] = 0x81;
+  buffer[1] = 0x04;
+  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
+  buffer[0] = 0x82;
+  buffer[1] = 0x80;
+  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
+  delay(30);
+  buffer[0] = 0x83;
+  buffer[1] = 0x01;
+  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
+  delay(25);
   buffer[0] = 0x85;
   buffer[1] = 0x01;
   this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
@@ -182,18 +197,6 @@ void MIPI_DSI_RPI::setup() {
   dsi_write(0x0104, 0x01);  // PPI_STARTPPI
   dsi_write(0x0204, 0x01);  // DSI_STARTDSI
   delay(100);
-  
-  buffer[0] = 0x81;
-  buffer[1] = 0x04;
-  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
-  buffer[0] = 0x82;
-  buffer[1] = 0x80;
-  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
-  delay(30);
-  buffer[0] = 0x83;
-  buffer[1] = 0x01;
-  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
-  delay(25);
   
   // set brightness max
   buffer[0] = 0x86;
