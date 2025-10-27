6f5f1739747232efeb0adebe26440d56df845de6
Oliver Kleinecke <okleinecke@web.de>
2025-10-27 02:14:16 +0100

    Update mipi_dsi_rpi.cpp

--- components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
+++ components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
@@ -101,11 +101,6 @@ void MIPI_DSI_RPI::setup() {
   buffer[0] = 0x85;
   this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
   delay(100);
-
-  buffer[0] = 0x85;
-  buffer[1] = 0x01;
-  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
-  delay(30);
   buffer[0] = 0x83;
   buffer[1] = 0x00;
   this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
@@ -183,7 +178,10 @@ void MIPI_DSI_RPI::setup() {
   buffer[1] = 0x01;
   this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
   delay(25);
-
+  buffer[0] = 0x85;
+  buffer[1] = 0x01;
+  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
+  delay(30);
   buffer[0] = 0x8D;
   buffer[1] = 0x04;
   this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
