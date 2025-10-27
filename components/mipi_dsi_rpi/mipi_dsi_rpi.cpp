8ed30f08b0821fba5117b7672f9e3ca573f61a3d
Oliver Kleinecke <okleinecke@web.de>
2025-10-27 03:17:58 +0100

    Update mipi_dsi_rpi.cpp

--- components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
+++ components/mipi_dsi_rpi/mipi_dsi_rpi.cpp
@@ -116,10 +116,6 @@ void MIPI_DSI_RPI::setup() {
   buffer[1] = 0x80;
   this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
   delay(30);
-  buffer[0] = 0x83;
-  buffer[1] = 0x01;
-  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
-  delay(25);
 
   // need to know when the display is ready for SLPOUT command - will be 120ms after reset
   auto when = millis() + 120;  
@@ -197,6 +193,12 @@ void MIPI_DSI_RPI::setup() {
   dsi_write(0x0104, 0x01);  // PPI_STARTPPI
   dsi_write(0x0204, 0x01);  // DSI_STARTDSI
   delay(100);
+
+  buffer[0] = 0x83;
+  buffer[1] = 0x01;
+  this->i2c_bus_->write_readv(this->i2c_address_, buffer, 2, nullptr, 0);
+  delay(25);
+  
   // set brightness max
   buffer[0] = 0x86;
   buffer[1] = 0xFF;
